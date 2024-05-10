e4c3caac5ae4428dff9df24b1d3d407860ba4708
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2024-05-09 22:17:04 -0400

    Add keyboard display and cleanup game state

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,15 @@ import (
 	"os"
 	"strings"
 
-	"github.com/bitmap/wordle-cli/internal/allowlist"
-	answers "github.com/bitmap/wordle-cli/internal/answers"
-	color "github.com/bitmap/wordle-cli/internal/color"
+	"github.com/bitmap/wordle-cli/internal/color"
+	"github.com/bitmap/wordle-cli/internal/words"
 )
 
 const wordLength = 5
 const totalGuesses int8 = 6
 const emptySpaceRune = '*'
 
-var correctAnswer = answers.RandomAnswer()
+var correctAnswer = words.RandomAnswer()
 
 type Guess struct {
 	value      rune
@@ -26,28 +25,33 @@ type Guess struct {
 
 // Game is a x * y grid
 type GameGrid [totalGuesses][wordLength]Guess
+type KeyboardMap map[rune]bool
+
+var gameGrid = GameGrid{[wordLength]Guess{}}
+var keyboardMap = KeyboardMap{}
+var keySlice = []rune("abcdefghijklmnopqrstuvwxyz")
 
 // Prompt the user to enter a string
-func guessPrompt(label string) string {
+func guessPrompt(input string) string {
 	var prompt string
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Fprint(os.Stderr, label+" ")
+		fmt.Fprint(os.Stderr, input+" ")
 		prompt, _ = reader.ReadString('\n')
 		if prompt != "" {
 			break
 		}
 	}
-	return strings.TrimSpace(prompt)
+	return strings.TrimSpace(strings.ToLower(prompt))
 }
 
 // Print the current state of the game
-func printGameGrid(grid GameGrid) {
-	for i := range grid {
+func printGameGrid() {
+	for i := range gameGrid {
 		fmt.Print("   ")
 
-		for j := range grid[i] {
-			currentChar := grid[i][j]
+		for j := range gameGrid[i] {
+			currentChar := gameGrid[i][j]
 
 			// Set the color of the character
 			runeColor := color.White
@@ -65,25 +69,61 @@ func printGameGrid(grid GameGrid) {
 	fmt.Println()
 }
 
-func main() {
-	fmt.Println("\n  Welcome to Wordle")
+func printKeyboard() {
+	fmt.Print("    ")
 
-	winFlag := false
+	// Print used keys a-m
+	for _, v := range keySlice[0:13] {
+		keyColor := color.White
+		if keyboardMap[v] {
+			keyColor = color.Gray
+		}
+		print(keyColor + string(v) + color.Reset)
+	}
 
-	grid := GameGrid{[wordLength]Guess{}}
+	fmt.Println()
+	fmt.Print("    ")
 
+	// Print used keys n-z
+	for _, v := range keySlice[13:] {
+		keyColor := color.White
+		if keyboardMap[v] {
+			keyColor = color.Gray
+		}
+		print(keyColor + string(v) + color.Reset)
+	}
+
+	fmt.Println()
+}
+
+func init() {
 	// Initialize the grid
-	for i := range grid {
-		for j := range grid[i] {
-			grid[i][j].value = emptySpaceRune
+	for i := range gameGrid {
+		for j := range gameGrid[i] {
+			gameGrid[i][j].value = emptySpaceRune
 		}
 	}
+
+	// Initialize the keyboard map. Keys from the keyboardMap are unsorted,
+	// so we just use the slice for display
+	for _, key := range keySlice {
+		keyboardMap[key] = false
+	}
+}
+
+func main() {
+	fmt.Println("\n  Welcome to Wordle")
+	winFlag := false
 
 	// Loop until we're out of guesses
 	var guessCount int8 = 0
 	for guessCount < totalGuesses {
-		printGameGrid(grid)
-		currentGuess := guessPrompt("  Your guess?")
+		// Print state of the game
+		printGameGrid()
+		printKeyboard()
+
+		// Get user input
+		currentGuess := guessPrompt("\n    Guess?>")
 
 		// Display an error if the user doesn't input enough chars
 		if len(currentGuess) != 5 {
@@ -92,20 +132,24 @@ func main() {
 		}
 
 		// Check to see if word is allowed
-		if !allowlist.IsValidWord(currentGuess) {
-			fmt.Println(color.Red + "\nInvalid word" + color.Reset)
+		if !words.IsValidWord(currentGuess) {
+			fmt.Println(color.Red + "\n    Invalid word!" + color.Reset)
 			continue
 		}
 
-		for index := range grid[guessCount] {
+		for index := range gameGrid[guessCount] {
+			char := currentGuess[index]
 			// Set the value to the corresponding rune char from the guess
-			grid[guessCount][index].value = rune(currentGuess[index])
+			gameGrid[guessCount][index].value = rune(char)
+
+			// Mark the character as used in the keyboard map
+			keyboardMap[rune(char)] = true
 
 			// Check if it's the same character or string contains rune at all
-			if correctAnswer[index] == currentGuess[index] {
-				grid[guessCount][index].isCorrect = true
-			} else if strings.ContainsRune(correctAnswer, rune(currentGuess[index])) {
-				grid[guessCount][index].isIncluded = true
+			if correctAnswer[index] == char {
+				gameGrid[guessCount][index].isCorrect = true
+			} else if strings.ContainsRune(correctAnswer, rune(char)) {
+				gameGrid[guessCount][index].isIncluded = true
 			}
 		}
 
@@ -119,8 +163,9 @@ func main() {
 		}
 	}
 
+	// Print final game state
 	fmt.Println("      Game Over")
-	printGameGrid(grid)
+	printGameGrid()
 
 	if winFlag {
 		if guessCount == 1 {
@@ -129,6 +174,6 @@ func main() {
 			fmt.Println("🎉 Correct! You won in " + fmt.Sprint(guessCount) + " guesses.")
 		}
 	} else {
-		fmt.Println("😓 Sorry, the answer was " + color.Cyan + correctAnswer + color.Reset + ". Try again.")
+		fmt.Println("😓 Sorry, the answer was " + color.Green + correctAnswer + color.Reset + ". Try again.")
 	}
 }
